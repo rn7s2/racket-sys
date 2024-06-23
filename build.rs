7f18ddcb4741f48055b0a996146eb4451f354979
rn7s2 <mail@rn7s2.cn>
2024-06-23 09:25:17 +0800

    Add MacOS support.

--- build.rs
+++ build.rs
@@ -1,13 +1,23 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::{env, fs};
+
+const MAC_DEFAULT_RKT: &str = "8.13";
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
     // directories
-    let rkt_home =
-        PathBuf::from(env::var("RACKET_CS_HOME").unwrap_or("C:/Program Files/Racket".to_string()));
+    let rkt_home = {
+        let home = env::var("RACKET_CS_HOME");
+        if cfg!(windows) {
+            PathBuf::from(home.unwrap_or("C:/Program Files/Racket".to_string()))
+        } else if cfg!(target_os = "macos") {
+            PathBuf::from(home.unwrap_or("/Applications/Racket v".to_string() + MAC_DEFAULT_RKT))
+        } else {
+            panic!("Platform is not supported yet!");
+        }
+    };
     let out_dir = {
         let mut path = PathBuf::from(env::var("OUT_DIR").unwrap());
         _ = path.pop() && path.pop() && path.pop();
@@ -43,11 +53,31 @@ fn main() {
         // link dynamic library
         println!("cargo:rustc-link-search={}", out_dir.display());
         println!("cargo:rustc-link-lib=libracketcs_dg1etc");
-
-        // cc::Build::new().file("empty.c").compile("racket-sys");
     } else if cfg!(target_os = "macos") {
+        let version = env::var("RACKET_CS_VERSION").unwrap_or(MAC_DEFAULT_RKT.to_string());
+        let lib_path = {
+            let mut path = rkt_home.clone();
+            path.push("lib");
+            path.push("Racket.framework");
+            path.push("Versions");
+            path.push(version + "_CS");
+            path.push("Racket");
+            path
+        };
+        let dest_lib_path = {
+            let mut path = out_dir.clone();
+            path.push("libracketcs.a");
+            path
+        };
+        fs::copy(lib_path, dest_lib_path).expect("Failed to find Racket library.");
+
+        // link library
+        println!("cargo:rustc-link-search={}", out_dir.display());
+        println!("cargo:rustc-link-lib=racketcs");
+
+        println!("cargo:rustc-link-lib=iconv");
+        println!("cargo:rustc-link-lib=ncurses");
         println!("cargo:rustc-link-lib=framework=CoreFoundation");
-        panic!("Platform is not supported yet!");
     } else {
         panic!("Platform is not supported yet!");
     }
@@ -77,6 +107,7 @@ fn main() {
 
     // copy dependencies to output directory
     // works even if this crate is a dependency of another crate
+    let boot_files = ["petite.boot", "scheme.boot", "racket.boot"];
     if cfg!(windows) {
         // generate lib file from module definition file
         let dll_path = {
@@ -92,7 +123,7 @@ fn main() {
         )
         .expect("Failed to copy dll file.");
 
-        let boot_files = ["petite.boot", "scheme.boot", "racket.boot"];
+        // copy boot files
         for boot_file in boot_files.iter() {
             let boot_path = {
                 let mut path = rkt_home.clone();
@@ -108,8 +139,60 @@ fn main() {
             .expect("Failed to copy boot file.");
         }
     } else if cfg!(target_os = "macos") {
-        panic!("Platform is not supported yet!");
+        // copy boot files
+        let version = env::var("RACKET_CS_VERSION").unwrap_or(MAC_DEFAULT_RKT.to_string());
+        for boot_file in boot_files.iter() {
+            let boot_path = {
+                let mut path = rkt_home.clone();
+                path.push("lib");
+                path.push("Racket.framework");
+                path.push("Versions");
+                path.push(version.clone() + "_CS");
+                path.push("boot");
+                path.push(boot_file);
+                path
+            };
+
+            fs::copy(
+                boot_path,
+                out_dir.to_str().unwrap().to_string() + "/" + boot_file,
+            )
+            .expect("Failed to copy boot file.");
+        }
+
+        // copy framework
+        let mut framework_dir = rkt_home.clone();
+        framework_dir.push("lib");
+        framework_dir.push("Racket.framework");
+
+        let mut out_dir = out_dir.clone();
+        out_dir.pop();
+        out_dir.pop();
+        copy_recursively(
+            framework_dir,
+            out_dir.to_str().unwrap().to_string() + "/Racket.framework",
+        )
+        .expect("Failed to copy Racket framework.");
     } else {
         panic!("Platform is not supported yet!");
     }
+}
+
+/// Copy files from source to destination recursively.
+/// thanks: https://nick.groenen.me/notes/recursively-copy-files-in-rust/
+pub fn copy_recursively(
+    source: impl AsRef<Path>,
+    destination: impl AsRef<Path>,
+) -> std::io::Result<()> {
+    fs::create_dir_all(&destination)?;
+    for entry in fs::read_dir(source)? {
+        let entry = entry?;
+        let filetype = entry.file_type()?;
+        if filetype.is_dir() {
+            copy_recursively(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
 }
