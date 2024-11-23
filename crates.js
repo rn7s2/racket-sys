9ce98aef468196eb48287d18fd09d291daf4b007
rn7s2 <mail@rn7s2.cn>
2024-11-24 03:55:46 +0800

    Update documents.

--- crates.js
+++ crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["racket_sys"];
+//{"start":21,"fragment_lengths":[12]}
--- src-files.js
+++ src-files.js
@@ -1,4 +1,3 @@
-var srcIndex = new Map(JSON.parse('[\
-["racket_sys",["",[],["lib.rs"]]]\
-]'));
+var srcIndex = new Map(JSON.parse('[["racket_sys",["",[],["lib.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[33]}
